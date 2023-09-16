99eab54d14c054aa15c29ac1ce830d64762bef7f
Luca <lgiallonardi@gmail.com>
2023-09-16 01:41:20 -0300

    configurando consumo de API y formulario

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['replicate.com', 'pbxt.replicate.delivery'],
+        },
+    };
 
 module.exports = nextConfig
